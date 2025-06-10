1b48796c3bb9395284f0828b3b38c54455f5e672
Masahiro Toma <143092368+mshr1-create@users.noreply.github.com>
2025-06-10 09:28:17 +0900

    ログイン画面のモックアップの作成

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
+import { LoginComponent } from './pages/login/login.component';
 // import { ModeSettingsComponent } from './mode-settings.component';
 // import { ProfileComponent } from './profile.component';
 // import { HistoryComponent } from './history.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
+    { path: 'login', component: LoginComponent },
     // { path: 'mode',       component: ModeSettingsComponent },
     // { path: 'profile',    component: ProfileComponent },
     // { path: 'history',    component: HistoryComponent },
---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule
+  ],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+}
