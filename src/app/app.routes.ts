a53aabfa6ee7661a4cfb13480c18cd79ea512745
Masahiro Toma <143092368+mshr1-create@users.noreply.github.com>
2025-06-11 23:13:28 +0900

    モード選択、時間設定画面のモックアップの作成

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
-// import { ModeSettingsComponent } from './mode-settings.component';
+import { ModeDurationComponent } from './pages/mode-duration/mode-duration.component';
 // import { ProfileComponent } from './profile.component';
 // import { HistoryComponent } from './history.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
-    // { path: 'mode',       component: ModeSettingsComponent },
+    { path: 'app-mode-duration', component: ModeDurationComponent },
     // { path: 'profile',    component: ProfileComponent },
     // { path: 'history',    component: HistoryComponent },
 ];
