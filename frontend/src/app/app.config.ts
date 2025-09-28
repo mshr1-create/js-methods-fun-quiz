22708571523371837ac4f4ea84a7cdd856747e4e
Masahiro Toma <143092368+mshr1-create@users.noreply.github.com>
2025-09-28 10:39:38 +0900

    fix: 重複部分を解消

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,15 +2,12 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
-import { bootstrapApplication } from '@angular/platform-browser';
-import { HomeComponent } from './home.component';
 import { authInterceptor } from './http/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes),
-    provideHttpClient(),
     provideHttpClient(withInterceptors([authInterceptor]))
   ]
 };
